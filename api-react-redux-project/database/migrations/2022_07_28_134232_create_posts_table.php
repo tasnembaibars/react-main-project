3afdda4b639d35d7eab794c1f31c4358cbe9bf1a
mohamadamarneh <mohamadamarneh7@gmail.com>
2022-07-28 17:29:28 +0300

    add laravel folder

---
+++ api-react-redux-project/database/migrations/2022_07_28_134232_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->nullable();
+            $table->string('location')->nullable();
+            $table->text('post')->nullable();
+            $table->foreignId('costumer_id')->nullable()->constrained('costumers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
