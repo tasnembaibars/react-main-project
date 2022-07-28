25590060ae8168ca4505733df020c42f1cecb621
AraaSmadi <100359539+AraaSmadi@users.noreply.github.com>
2022-07-28 17:24:34 +0300

    react

--- event/src/App.js
+++ event/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link ,NavLink } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-
+import Index from './components/Index';
 import Contact from './components/contact';
 import Register from './components/Register';
 import Login from './components/Login';
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <NavBar />
     <Routes>
-
+    <Route path="/" element={<Index />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
