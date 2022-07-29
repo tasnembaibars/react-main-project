3d6f81f62e8c48512ea3afa532f0da61e529edd5
hayaabdallah <100359839+hayaabdallah@users.noreply.github.com>
2022-07-29 14:34:08 +0100

    editprofile

--- event/src/App.js
+++ event/src/App.js
@@ -1,38 +1,26 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link ,NavLink } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import Home from './components/Home';
-import Contact from './contact';
-import About from './components/About';
-import Single from './components/Single';
-import Shop from './components/Shop';
 import Index from './components/Index';
 import Contact from './components/contact';
 import Register from './components/Register';
 import Login from './components/Login';
 import Profile from './components/Profile';
+
+
 function App() {
   return (
 
     <Router>
-      
       <NavBar />
     <Routes>
     <Route path="/" element={<Index />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
-
-
-
-
-{/* routes */}
-<Route path="/about" element={<About />} />
-<Route path="/post" element={<Single />} />
-<Route path="/shop" element={<Shop />} />
+      <Route path="/profile/:id" element={<Profile/>} />
 
     </Routes>
     <Footer />
--- event/src/components/Profile.js
+++ event/src/components/Profile.js
@@ -1,135 +1,166 @@
-import React from 'react'
+import './profile.css';
+import {useParams} from 'react-router';
+import { Link } from "react-router-dom";
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 
 function Profile() {
+
+    const[users,setUsers]=useState({
+        name:'',email:'',password:''
+      });
+  
+    const[update,setUpdate]=useState(false);
+    const {id}=useParams();
+    console.log(id);
+
+    useEffect(()=>{
+        const data=async()=>{
+             const response=await fetch(`http://127.0.0.1:8000/api/profile/${id}`)
+             const dbData=await response.json();
+             setUsers(dbData);
+               
+        }
+        data()
+      },[update])
+
+      const updateHandeler=(e)=>{
+        e.preventDefault();
+        axios.put(`http://127.0.0.1:8000/api/profile/${id}`, users)
+        .then((res)=>setUsers(res.data))
+        .then(()=>setUpdate(!update))
+        console.log(users)
+        if(!update){
+         window.alert('Your profile has been updated successfuly')
+               
+        }}
+
+
+
+
+
+
   return (
 
 
-<div>
-      <div className="container" style={{ marginTop: '150px' }}>
-        <div className="main-body">
-
-          {/* <nav aria-label="breadcrumb" className="main-breadcrumb">
-            <ol className="breadcrumb">
-              <li className="breadcrumb-item"><a href="index.html">Home</a></li>
-              <li className="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
-              <li className="breadcrumb-item active" aria-current="page">User Profile</li>
-            </ol>
-          </nav> */}
-
-
-          <div className="row gutters-sm" style={{ backgroundColor: 'white' }}>
-            <div className="col-md-4 mb-3">
-              <div className="card" style={{ backgroundColor: '#edf3f8' }}>
-                <div className="card-body">
-                  <div className="d-flex flex-column align-items-center text-center">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150"></img>
-                    <div className="mt-3">
-                      {/* <h4>{user.name}</h4> */}
-                      <div className="row">
-                        <div className="col-sm-3" style={{ width: '320px', marginLeft: '70px', marginTop: '20px' }}>
-                          {/* <button class="book-a-table-btn scrollto d-none d-lg-flex " target="__blank" ><Link to={{ pathname: `/EditProfile/${ID}` }} >Edit Your Profile </Link></button> */}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
+<div class="container" style={{marginTop:'150px'}}>
+<div class="profile-wrapper">
+    <div class="profile-section-user">
+        {/* <div class="profile-cover-img"><img src="https://bootdey.com/img/Content/flores-amarillas-wallpaper.jpeg" alt=""/></div> */}
+        <div class="profile-info-brief p-3"><img class="img-fluid user-profile-avatar" src="https://bootdey.com/img/Content/avatar/avatar6.png" alt=""/>
+            <div class="text-center">
+                {/* <h5 class="text-uppercase mb-4">John Doe</h5> */}
             </div>
-            <div className="col-md-8" style={{ backgroundColor: '#edf3f8' }}>
-              <div className="card mb-3" style={{ backgroundColor: 'white', marginTop: '20px' }}>
-                <h3>Personal Information</h3>
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-sm-3">
-
-                      <h6 className="mb-0">Full Name </h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {/* {user.name} */}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Email</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {/* {user.email} */}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Phone</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {/* {user.phone} */}
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="row">
-                    <div className="col-sm-3">
-                      <h6 className="mb-0">Password</h6>
-                    </div>
-                    <div className="col-sm-9 text-secondary">
-                      {'**************'}
-                    </div>
-                  </div>
-
-
-                </div>
-              </div>
-
-
-
-
-
-            </div>
-          </div>
-
         </div>
-      </div>
 
-      <div class="col-md-12" style={{ marginLeft: '280px', width: '70%', marginTop: '40px' ,marginBottom:'50px'}}>
-                   <h3> History</h3>
-                   <div className="row">
-                       <table class="table table-secondery" style={{ color: ' #6c757d' }}>
-                           <thead>
-                               <tr>
-                                   <th scope="col">#</th>
-                                   <th scope="col">Table</th>
-                                   <th scope="col">Date</th>
-                                   <th scope="col">Time</th>
-                               </tr>
-                           </thead>
-                           <tbody>
-                               <tr>
-                                   <th scope="row">1</th>
-                                   <td>Mark</td>
-                                   <td>Otto</td>
-                                   <td>@mdo</td>
-                               </tr>
-                               <tr>
-                                   <th scope="row">2</th>
-                                   <td>Jacob</td>
-                                   <td>Thornton</td>
-                                   <td>@fat</td>
-                               </tr>
-                               <tr>
-                                   <th scope="row">3</th>
-                                   <td>Larry</td>
-                                   <td>the Bird</td>
-                                   <td>@twitter</td>
-                               </tr>
-                           </tbody>
-                       </table>
-                   </div>
-               </div>
+        <hr class="m-0"/>
+        <div class="hidden-sm-down">
+            <hr class="m-0"/>
+            <div class="profile-info-contact p-4">
+                <h6 class="mb-3">User Information</h6>
+                <form onSubmit={updateHandeler}>
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <td><strong>Name:</strong></td>
+                            <td>
+                            <input type="text" className="form-control" name='name' value={users.name}  onChange={(e)=>setUsers({...users,name:e.target.value})} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><strong>Email:</strong></td>
+                            <td>
+                                {/* <p class="text-muted mb-0">rathemes@gmail.com</p> */}
+                                <input type="text" className="form-control" name='email' value={users.email}  onChange={(e)=>setUsers({...users,email:e.target.value})}/>
+                            </td>
+                        </tr>
+                        
+                        <tr>
+                            <td><strong>Password:</strong></td>
+                            <td>
+                                {/* <p class="text-muted mb-0">Rathemes</p> */}
+                                <input type="password" className="form-control" name='password' placeholder='' value={users.password}    onChange={(e)=>setUsers({...users,password:e.target.value})} />
+                            </td>
+                        </tr>
+                        <button className="btn btn-primary" type='submit' style={{backgroundColor:'#86a0b6',marginTop:'10px'}} >Update</button>
+                        {/* <button class="book-a-table-btn scrollto d-none d-lg-flex " target="__blank" ><Link to={{ pathname: `/EditProfile/` }} >Edit Your Profile </Link></button> */}
+                        {/* <button class="book-a-table-btn scrollto d-none d-lg-flex " target="__blank" ><Link to={{ pathname: `/EditProfile/${ID}` }} >Edit Your Profile </Link></button> */}
+                    </tbody>
+                </table>
+                </form>
+            </div>
+
+            <hr class="m-0"/>
+            {/* <div class="profile-info-general p-4">
+                <h6 class="mb-3">General Information</h6>
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <td><strong>JOB:</strong></td>
+                            <td>
+                                <p class="text-muted mb-0">Web Developer</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><strong>POSITION:</strong></td>
+                            <td>
+                                <p class="text-muted mb-0">Team Manager</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><strong>STUDIED:</strong></td>
+                            <td>
+                                <p class="text-muted mb-0">Computer Science</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><strong>LAST SEEN:</strong></td>
+                            <td>
+                                <p class="text-muted mb-0">Yesterday 8:00 AM</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div> */}
+           
+            <hr class="m-0"/>
+        </div>
+ 
     </div>
 
+    <div class="profile-section-main">
+     
+        <ul class="nav nav-tabs profile-tabs" role="tablist">
+            <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#profile-overview" role="tab">Timeline</a></li>
+        </ul>
+    
+        <div class="tab-content profile-tabs-content">
+            <div class="tab-pane active" id="profile-overview" role="tabpanel">
+            
+                
+                <div class="stream-posts">
+                    <div class="stream-post">
+                        <div class="sp-author">
+                            <a href="#" class="sp-author-avatar"><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt=""/></a>
+                            <h6 class="sp-author-name"><a href="#">John Doe</a></h6></div>
+                        <div class="sp-content">
+                            <div class="sp-info">posted 1h ago</div>
+                            <p class="sp-paragraph mb-0">Auk Soldanella plainscraft acetonylidene wolfishness irrecognizant Candolleaceae provision Marsipobranchii arpen Paleoanthropus supersecular inidoneous autoplagiarism palmcrist occamy equestrianism periodontoclasia mucedin overchannel goelism decapsulation pourer zira</p>
+                        </div>
+                     
+                    </div>
         
+                </div>
+           
+            </div>
+        </div>
+   
+    </div>
+   
+</div>
+</div>
         
+
 
   )
 }
