b7bb1d32508588918e1c4c161f47f551648e72c8
tasnembaibars <tasnembaibars@gmail.com>
2022-07-29 21:03:13 +0300

    main

--- api-react-redux-project/routes/api.php
+++ api-react-redux-project/routes/api.php
@@ -46,5 +46,5 @@ Route::get('/profile/{id}',[CostumersController::class,'edit']);
 // Route::post('/post',[CommentsPostController::class,'store']);
 Route::post('/post',[PostsController::class,'store']);
 Route::get('/posts',[PostsController::class,'create']);
-Route::post('/post',[CommentsPostController::class,'store']);
+Route::post('/comments_post',[CommentsPostController::class,'store']);
 
