27a21fd1ac4dc6512e5eb606375feeb791c185bc
AraaSmadi <100359539+AraaSmadi@users.noreply.github.com>
2022-07-29 17:49:05 +0300

    react

--- api-react-redux-project/routes/api.php
+++ api-react-redux-project/routes/api.php
@@ -2,16 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
+
 use App\Http\Controllers\CostumersController;
 
-=======
 use App\Http\Controllers\CommentsPostController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\PostsController;
-=======
->>>>>>> f1bd17273a9da65094ca9361e501d578847acc63
->>>>>>> eb411ca52398c37adb9c85b011b73d536b6ce2ca
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,14 +24,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
+
 Route::get('/profile/{id}',[CostumersController::class,'index']);
 
 //edit
 Route::post('/profile',[CostumersController::class,'store']);
 Route::put('/profile/{id}',[CostumersController::class,'update']);
 Route::get('/profile/{id}',[CostumersController::class,'edit']);
-=======
 
 
 
@@ -44,11 +40,8 @@ Route::get('/profile/{id}',[CostumersController::class,'edit']);
 
 
 // comment on post
-<<<<<<< HEAD
+
 // Route::post('/post',[CommentsPostController::class,'store']);
 Route::post('/post',[PostsController::class,'store']);
 Route::get('/posts',[PostsController::class,'create']);
-=======
 Route::post('/post',[CommentsPostController::class,'store']);
->>>>>>> f1bd17273a9da65094ca9361e501d578847acc63
->>>>>>> eb411ca52398c37adb9c85b011b73d536b6ce2ca
