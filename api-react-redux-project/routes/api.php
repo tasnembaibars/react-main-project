7fcebc5414cecb870c0064df5c11d36f9fc0a43e
hayaabdallah <100359839+hayaabdallah@users.noreply.github.com>
2022-07-29 15:57:34 +0100

    main

--- api-react-redux-project/routes/api.php
+++ api-react-redux-project/routes/api.php
@@ -2,12 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\CostumersController;
 
-=======
 use App\Http\Controllers\CommentsPostController;
->>>>>>> f1bd17273a9da65094ca9361e501d578847acc63
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,14 +20,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
 Route::get('/profile/{id}',[CostumersController::class,'index']);
 
 //edit
 Route::post('/profile',[CostumersController::class,'store']);
 Route::put('/profile/{id}',[CostumersController::class,'update']);
 Route::get('/profile/{id}',[CostumersController::class,'edit']);
-=======
 
 
 
@@ -41,4 +36,3 @@ Route::get('/profile/{id}',[CostumersController::class,'edit']);
 
 // comment on post
 Route::post('/post',[CommentsPostController::class,'store']);
->>>>>>> f1bd17273a9da65094ca9361e501d578847acc63
