f7ccb00af8b63089a04a25fbecc888578a26541d
hayaabdallah <100359839+hayaabdallah@users.noreply.github.com>
2022-07-31 14:31:45 +0100

    main

--- api-react-redux-project/app/Http/Controllers/CostumersController.php
+++ api-react-redux-project/app/Http/Controllers/CostumersController.php
@@ -177,9 +177,6 @@ class CostumersController extends Controller
 
 
 
-
-
-
       //selects users data for comments page
       public function view($id){
        return Costumers::all()->where('id',$id);
